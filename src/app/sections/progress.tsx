3cc671f670a18e90b44885e88fe5f999c9e60a80
Miki Stec <mikulas@stec.sk>
2025-04-24 12:49:52 +0800

    Content: info about sneak peak and adjust hit me up section

--- src/app/sections/progress.tsx
+++ src/app/sections/progress.tsx
@@ -1,4 +1,6 @@
+
 import { Chilanka } from "next/font/google";
+import Link from "next/link";
 
 const indieFlower = Chilanka({
     subsets: ['latin'],
@@ -11,9 +13,9 @@ export const Progress = () => {
         <div className={`${indieFlower.className} bg-white md:m-16 m-6 shadow-xl transform md:rotate-2 text-center md:p-16 p-6 max-w-[900px] space-y-6`}>
             <h2 className="text-2xl font-bold tracking-wide sm:text-3xl">Behind the Scenes</h2>
             <p className="text-xl">
-                If you&apos;re on the indie path too, maybe we can team up! A buddy is never a bad thing—whether it&apos;s sharing know-how, equipment, or just giving feedback. Let&apos;s create something amazing together! I&apos;ve spent my whole life in music, and since 2003, I&apos;ve been diving deep into DAWS and computers. Recently, I&apos;ve been exploring the latest tools, hardware, and sound libraries. I&apos;ve got quite a lot to offer.
+                If you&apos;re on the indie path too, maybe we can team up! A buddy is never a bad thing—whether it&apos;s sharing know-how, equipment, or just giving feedback. Let&apos;s create something amazing together! I&apos;ve spent my whole life in music, and since 2003, I&apos;ve been diving deep into DAWS and computers. Recently, I&apos;ve been exploring the latest tools, hardware, and sound libraries. I&apos;ve got quite a lot to offer. <Link href="#contact" className="underline underline-offset-4">Hit me up!</Link>
             </p>
-            <p className="text-2xl font-bold mt-2">Indie is never really Indie</p>
+            {/* <p className="text-2xl font-bold mt-2">Indie ≠ Indie & Solo ≠ Solitary — Freedom in creation, but never alone.</p> */}
         </div>
 
 
--- src/app/sections/work.tsx
+++ src/app/sections/work.tsx
@@ -5,11 +5,15 @@ export const Work = () => {
     return (
         <section id="work" className="w-full py-12 md:py-24 lg:py-32 bg-white/40">
             <div className="container px-4 md:px-6">
-                <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                    <div className="space-y-2">
-                        <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl text-purple-900"> Sneak peek</h2>
-                        <p className="max-w-[900px] text-purple-800/80">
+                <div className="flex flex-col items-center justify-center space-y-4">
+                    <div className="space-y-4 mb-6">
+                        <h2 className=" text-center text-2xl font-bold tracking-tighter sm:text-3xl text-purple-900"> Sneak peek</h2>
+                        <p className="max-w-[900px] text-purple-800/80 text-center italic">
                             A selection of some unmastered mixes
+                        </p>
+                        <p className="max-w-[900px] text-purple-800/80">
+                            These are some initial experiments after my return to production. I&apos;ve been shaking off the rust and getting back into the flow, but I didn&apos;t have a proper mic or warmed-up vocal cords at the time, so they&apos;re more like audio demos with no singing yet.
+                            <br></br>The first track, <span className="italic font-bold">however</span>, is probably going on the album as is. It&apos;s all about how having too many options can be paralyzing—sometimes, you just have to choose one and go with it.
                         </p>
                     </div>
                     <Player></Player>
